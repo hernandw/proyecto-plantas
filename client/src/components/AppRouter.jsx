dbf0d781cc5b51380c0038f7bf0989de128c2416
Ana Belen <abilabae@gmail.com>
2023-08-22 21:11:43 -0400

    mejora del footer

--- client/src/components/AppRouter.jsx
+++ client/src/components/AppRouter.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import HomePage from '../pages/HomePage'
-import Favoritos from '../pages/Favoritos'
 import Details from '../pages/Details'
 import Login from '../pages/Login'
 import Register from '../pages/Register'
@@ -12,7 +11,6 @@ const AppRouter = () => {
     <div>
         <Routes>
             <Route path='/' element={<HomePage />} />
-            <Route path='favoritos' element={<Favoritos />} />
             <Route path='product/:id' element={<Details />} />
             <Route path='login' element={<Login />} />
             <Route path='register' element={<Register />} />

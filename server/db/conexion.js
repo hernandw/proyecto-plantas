cc648e5edf3e9eda30a8ec12575d0c6995788553
camilo aliaga <camilo.aliaga.silva@gmail.com>
2023-08-24 21:15:48 -0400

    conectando backend con render

--- server/db/conexion.js
+++ server/db/conexion.js
@@ -2,11 +2,15 @@ const {Pool}= require('pg')
 require('dotenv').config()
 
 const pool= new Pool ({
-    user: process.env.DB_USER,
+    /* user: process.env.DB_USER,
     host:process.env.DB_HOST,
     database:process.env.DB_DATABASE,
     password:process.env.DB_PASSWORD,
     allowExitOnIdle: true
+ */
+
+connectionString: process.env.DATABASE_URL
+
 })
 
 module.exports=pool
